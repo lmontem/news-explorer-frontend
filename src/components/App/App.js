65a81fd9b1f106c6ecff7d7e0c6ed9bfa097ba25
lmontem <lmontem7@gmail.com>
2021-06-03 10:57:47 -0400

    preloader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import Header from '../Header/Header.js'
 import SearchForm from '../SearchForm/SearchForm.js';
+import Preloader from '../Preloader/Preloader';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
     <div className="page">
       {/*<Header />*/}
       <SearchForm />
+      <Preloader />
       <About />
       <Footer />
     </div>
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+function Preloader(){
+    return(
+        (
+            <section className= 'preloader'>
+                <div className='preloader__circle'></div>
+                <p className='preloader__message'>Searching for news...</p>
+            </section>
+        )
+    )
+}
+export default Preloader;
