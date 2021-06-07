7d3d9394d1749f56af9426e0e56a337565f0b502
lmontem <lmontem7@gmail.com>
2021-06-07 12:06:29 -0400

    media queires/head.nav.sear.sav

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -9,7 +9,7 @@ function SearchForm(props) {
                     <h1 className='search__header'>What's going on in the world?</h1>
                     <p className='search__text'>Find the latest news on any topic and save them in your personal account.</p>
                     <form className='search__form'>
-                        <input className='search__input' />
+                        <input className='search__input' placeholder='Enter topic' />
                         <button className='search__btn'>Search</button>
                     </form>
                 </section>
