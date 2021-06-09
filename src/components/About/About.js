ef3a795882a4b6b190591382701bb18d42c308fb
lmontem <lmontem7@gmail.com>
2021-06-09 15:24:04 -0400

    media queries/ about/footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,15 @@
-function About(props){
-    return(
+import author from '../../images/leandra.jpg';
+function About(props) {
+    return (
         (
             <section className='about'>
-                <img className='about__img' src='' alt='the author' />
-                <h2 className='about__heading'>About the author</h2>
-                <p className='about__description'>This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
+                <img className='about__img' src={author} alt='the author' />
+                <div className='about__container'>
+                    <h2 className='about__heading'>About the author</h2>
+                    <p className='about__description'>This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
 
 You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.</p>
-                
+                </div>
             </section>
         )
     )
